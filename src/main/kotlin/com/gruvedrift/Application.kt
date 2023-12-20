b2d63d19a3069b57eca07ae82457c987f3d9e5e2
gruvedrift <william.gruvedrift@gmail.com>
2023-12-20 22:56:29 +0100

    First commit, nothing exciting...

---
+++ src/main/kotlin/com/gruvedrift/Application.kt
@@ -0,0 +1,13 @@
+package com.gruvedrift
+
+import com.gruvedrift.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSockets()
+    configureRouting()
+}
